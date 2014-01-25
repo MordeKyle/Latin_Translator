7a0883e4989d3dbb5b58f0601f560544b1127380
Kyle McBride <Kyle@MordeKyle.com>
2014-01-25 04:37:49 -0600

    Finaler

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,13 @@
             this.mediumButton = new System.Windows.Forms.Button();
             this.sinisterButton = new System.Windows.Forms.Button();
             this.answerLabel = new System.Windows.Forms.Label();
+            this.identLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // questionLabel
             // 
             this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.questionLabel.Location = new System.Drawing.Point(6, 59);
             this.questionLabel.Name = "questionLabel";
             this.questionLabel.Size = new System.Drawing.Size(270, 13);
@@ -85,11 +87,21 @@
             this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.answerLabel.Click += new System.EventHandler(this.answerLabel_Click);
             // 
+            // identLabel
+            // 
+            this.identLabel.Location = new System.Drawing.Point(12, 240);
+            this.identLabel.Name = "identLabel";
+            this.identLabel.Size = new System.Drawing.Size(260, 23);
+            this.identLabel.TabIndex = 5;
+            this.identLabel.Text = "By: Kyle McBride A02609917 for IS 107 602";
+            this.identLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.identLabel);
             this.Controls.Add(this.answerLabel);
             this.Controls.Add(this.sinisterButton);
             this.Controls.Add(this.mediumButton);
@@ -110,6 +122,7 @@
         private System.Windows.Forms.Button mediumButton;
         private System.Windows.Forms.Button sinisterButton;
         private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Label identLabel;
 
     }
 }
